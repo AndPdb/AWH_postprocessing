b72608fd371491c829c71e11781c2e312089fd91
AndPdb <and.pdb92@gmail.com>
2024-05-23 14:52:41 +0200

    Add dimensionality to prepare_pullx

--- prepare_pullx.py
+++ prepare_pullx.py
@@ -3,6 +3,7 @@ import numpy as np
 from multiprocessing import Process
 
 WORKING_DIR = "test/1D"
+dim = 1
 
 #Parallelize one walker per each core
 def runInParallel(walkers):
@@ -24,10 +25,21 @@ def concat_xvg(directory):
     sorted_list = sorted(lsdir, key=lambda x: (x.split('.')[1].split('_')[0]))
 
     #Read xvg and append the array in a list
-    for xvg in sorted_list:
-        xvglist.append(np.loadtxt(os.path.join(directory, xvg), comments=['#','@'], usecols=[-2, -1]))
+    if dim == 1 :
+    
+      for xvg in sorted_list:
+          xvglist.append(np.loadtxt(os.path.join(directory, xvg), comments=['#','@'], usecols=[-1]))
 
-    np.save(os.path.join(directory,f"pullx_{len(lsdir)}.npy"), np.concatenate((xvglist), axis=0)) #concatenate all the arrays
+      np.save(os.path.join(directory,f"pullx_{len(lsdir)}.npy"), np.concatenate((xvglist), axis=0)) #concatenate all the arrays
+    
+    elif dim == 2 :
+        for xvg in sorted_list:
+          xvglist.append(np.loadtxt(os.path.join(directory, xvg), comments=['#','@'], usecols=[-2, -1]))
+
+        np.save(os.path.join(directory,f"pullx_{len(lsdir)}.npy"), np.concatenate((xvglist), axis=0)) #concatenate all the arrays
+    
+    else:
+       print("Postprocessing implemented just for dimensions <= 2")
 
     return 1
 
